b9ce740343dbe8d003a415d2cc9e62cf41aadb2e
NazarovAsadbek <nazarov_asadbek_official@mail.ru>
2023-02-17 18:35:39 +0500

    Added review controller

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from "@nestjs/common";
+import { ReviewModel } from "./review.model";
 
-@Controller('review')
-export class ReviewController {}
+@Controller("review")
+export class ReviewController {
+
+  @Post("create")
+  async create(@Body() dto: Omit<ReviewModel, "_id">) {
+    return "Review created";
+  }
+
+  @Delete(":id")
+  async delete(@Param("id") id: string) {
+    return "Review deleted";
+  }
+
+  @Get("byProduct/:productId")
+  async getByProduct(@Param("productId") productId: string) {
+    return "Reviews for product";
+  }
+
+}
